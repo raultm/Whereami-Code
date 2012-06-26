f0e040e11219eb3c8c17b08ebdf0c1480d38e9b7
Raul Tierno <raultm@gmail.com>
2012-06-26 20:31:58 +0200

    Start the project with CoreLocation Library include

---
+++ Whereami/WhereamiViewController.h
@@ -0,0 +1,14 @@
+//
+//  WhereamiViewController.h
+//  Whereami
+//
+//  Created by Raul Tierno Magro on 26/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface WhereamiViewController : UIViewController
+
+@end
---
+++ Whereami/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Whereami
+//
+//  Created by Raul Tierno Magro on 26/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WhereamiAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WhereamiAppDelegate class]));
+    }
+}
