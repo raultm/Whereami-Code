ffac1972ab911066ab0680ef8ba21b67f6cd9941
Raul Tierno <raultm@gmail.com>
2012-06-27 08:58:36 +0200

    Declare Controller conforms to the CLLocationManagerDelegate

--- Whereami/WhereamiViewController.h
+++ Whereami/WhereamiViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 
-@interface WhereamiViewController : UIViewController
+@interface WhereamiViewController : UIViewController <CLLocationManagerDelegate>
 {
     CLLocationManager *locationManager;
 }
